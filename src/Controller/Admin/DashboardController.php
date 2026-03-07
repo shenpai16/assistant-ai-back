4558fb690ff5160f75d047f85812daa8ed176059
Raphael Benmimoune <raphaelbenmimoune1@gmail.com>
2026-03-07 12:29:31 +0100

    amelioration du design pour le dashboard admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,22 +2,54 @@
 
 namespace App\Controller\Admin;
 
-use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use Symfony\Component\HttpFoundation\Response;
-
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use App\Entity\Company;
+use App\Entity\Conversation;
+use App\Entity\Message;
 use App\Controller\Admin\CompanyCrudController;
 use App\Controller\Admin\ConversationCrudController;
 use App\Controller\Admin\MessageCrudController;
 
+use Doctrine\Persistence\ManagerRegistry;
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+
+
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
 {
+    private ManagerRegistry $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
     public function index(): Response
     {
-        return $this->redirectToRoute('admin_company_index');
+        $companyRepo = $this->doctrine->getRepository(Company::class);
+        $conversationRepo = $this->doctrine->getRepository(Conversation::class);
+        $messageRepo = $this->doctrine->getRepository(Message::class);
+
+        $companyCount = $companyRepo->count([]);
+        $conversationCount = $conversationRepo->count([]);
+        $messageCount = $messageRepo->count([]);
+
+        $messageLast24h = $messageRepo->createQueryBuilder('m')
+            ->select('COUNT(m.id)')
+            ->where('m.createdAt >= :date')
+            ->setParameter('date', new \DateTimeImmutable('-24 hours'))
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        return $this->render('admin/dashboard.html.twig', [
+            'companyCount' => $companyCount,
+            'conversationCount' => $conversationCount,
+            'messageCount' => $messageCount,
+            'messageLast24h' => $messageLast24h,
+        ]);
     }
 
     public function configureDashboard(): Dashboard
